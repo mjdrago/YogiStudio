185109ab7bd367d4652f27cc2e4d1a38d4bee6a5
sehenry <sarahelhe@gmail.com>
2018-04-25 18:13:02 -0500

    Visual Studio file created for Yogi Studio

---
+++ YogiStudio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YogiStudio.Startup))]
+namespace YogiStudio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
