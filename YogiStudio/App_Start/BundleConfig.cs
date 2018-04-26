4b0b9515950f95b009456d80d2422b086e708a6a
sehenry <sarahelhe@gmail.com>
2018-04-25 20:59:59 -0500

    Updated CSS to use bootstrap-minty.css

--- YogiStudio/App_Start/BundleConfig.cs
+++ YogiStudio/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace YogiStudio
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-minty.css",
                       "~/Content/site.css"));
         }
     }
