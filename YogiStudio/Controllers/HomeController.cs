dd7266ad8ae5fa9f32132f2b2f9ab652fd06b1f5
mjdrago <mario.j.drago@gmail.com>
2018-04-30 22:45:04 -0500

    Added structure so that home button goes to different Home pages depending on the role.

--- YogiStudio/Controllers/HomeController.cs
+++ YogiStudio/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YogiStudio.Models;
 
 namespace YogiStudio.Controllers
 {
@@ -10,6 +13,28 @@ namespace YogiStudio.Controllers
     {
         public ActionResult Index()
         {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+
+                if (s[0].ToString() == "Admin")
+                {
+                    return View();
+                }
+                else if (s[0].ToString() == "Customer")
+                {
+                    return View();
+                }
+                else if (s[0].ToString() == "Instructor")
+                {
+                    return View();
+                }
+            }
             return View();
         }
 
